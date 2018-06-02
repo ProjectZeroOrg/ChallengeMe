a5c659232f9d25555b810fa421154f991c38c82e
Iliyan Mihov <ilian.mihov@gmail.com>
2018-06-02 13:02:58 +0300

    Add authentication

--- src/main/java/org/projectzero/challengeme/ChallengeMeApplication.java
+++ src/main/java/org/projectzero/challengeme/ChallengeMeApplication.java
@@ -1,12 +1,39 @@
 package org.projectzero.challengeme;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class ChallengeMeApplication {
+@EnableOAuth2Sso
+public class ChallengeMeApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeMeApplication.class, args);
 	}
+
+  @Value("${security.trusted-ssl-hosts:46.249.93.103}")
+	public void setTrustedSslHosts(String trustedSslHosts) {
+    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+      (hostname, sslSession) -> Arrays.stream(trustedSslHosts.split(","))
+        .anyMatch(hostname::contains));
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // @formatter:off
+    http
+      .logout().and()
+      .authorizeRequests()
+        .antMatchers("/", "/login", "/index.html",
+          "/*.js", "/*.css", "/*.ico", "/*.png", "/*image**")
+        .permitAll()
+        .anyRequest().authenticated();
+    // @formatter:on
+  }
 }
